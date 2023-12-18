00952cf769e4e0849e84f246e67f039b564ae03c
cdixo <114014537+cdixo@users.noreply.github.com>
2023-12-18 16:00:36 -0500

    Delete RNA-seq/Read Preparation directory

--- RNA-seq/Read Preparation/Read Prep Pt 3[1] - RNAseq_fastQC_after.sh
+++
@@ -1,6 +0,0 @@
-/users/PAS1444/li10917/miniconda2/envs/rnaseq/bin/fastqc -t 4 -f fastq *clean.fq
-
-mkdir fastQC_output_after
-
-mv *fastqc.html fastQC_output_after
-mv *fastqc.zip fastQC_output_after
